6161068272de67215ab9ac58c93e5f9d22aa26ba
gaurang <gaurang8102003@gmail.com>
2023-10-05 18:59:51 +0530

    cart backend doen

--- server/bookstore/carts/models.py
+++ server/bookstore/carts/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from books.models import Book
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from decimal import Decimal
 
 # Create your models here.
 
@@ -23,18 +24,33 @@ class CartItem(models.Model):
     def __str__(self):
         return self.book.title
     
+    def save(self, *args, **kwargs):
+
+        quantity_decimal = Decimal(str(self.quantity))
+        book_price_decimal = Decimal(str(self.book.price))
+
+        self.book_price_total = quantity_decimal * book_price_decimal
+        super().save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        self.cart.amount -= self.book_price_total
+        self.cart.save()
+        super().delete(*args, **kwargs)
+    
+
+
 @receiver(post_save, sender=User)
 def create_user_cart(sender, instance, created, **kwargs):
     if created:
         Cart.objects.create(user=instance)
         
+
+
 @receiver(post_save, sender=CartItem)
 def update_cart_total(sender, instance, created, **kwargs):
-    if created:
-        cart = instance.cart
-        cart_item_total = instance.quantity * instance.book.price
-        cart.amount += cart_item_total
-        cart.save()
 
-        instance.book_price_total = cart_item_total
-        instance.save()
+    if not created:
+        cart = instance.cart
+        cart_items = CartItem.objects.filter(cart=cart)
+        cart.amount = sum(item.book_price_total for item in cart_items)
+        cart.save()
